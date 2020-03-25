3d67c0bc414d76e24a55272ff47d1b3ca84428be
AskanHolaAE17 <be.in.pair@gmail.com>
2020-03-25 20:38:11 +0200

    CRUD pages and actions for Courier

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -2,7 +2,51 @@
 
 # Controller to see the list of Couriers
 class CouriersController < ApplicationController
+  before_action :set_courier, only: %i[show edit update destroy]
+
   def index
     @couriers = Courier.all
+  end
+
+  def new
+    @courier = Courier.new
+  end
+
+  def create
+    @courier = Courier.new(courier_params)
+
+    if @courier.save
+      redirect_to @courier, notice: "Запис про кур'єра було успiшно створено."
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @courier.update(courier_params)
+      redirect_to @courier, notice: "Запис про кур'єра було успiшно видалено."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @courier.destroy
+    redirect_to couriers_url, notice: "Запис про кур'єра було успiшно видалено."
+  end
+
+  private
+
+  def set_courier
+    @courier = Courier.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def courier_params
+    params.require(:courier).permit(:name, :email)
   end
 end
--- app/models/courier.rb
+++ app/models/courier.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
+# Model for Courier
 class Courier < ApplicationRecord
+
+  validates :name,    presence: true
+  validates :email,   presence: true
+  
 end
