48011fa0924cabf652050408efda061194a64a3d
AskanHolaAE17 <be.in.pair@gmail.com>
2020-03-25 13:50:30 +0200

    Add Courier model

---
+++ db/migrate/20200325114630_create_couriers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateCouriers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :couriers do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
